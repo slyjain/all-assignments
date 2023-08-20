79503f6b0ad0de7681884f4814be24e9fad61cef
slyjain <125951111+slyjain@users.noreply.github.com>
2023-08-20 15:37:02 +0530

    Update anagram.js

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -6,9 +6,21 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
+function isAnagram(str1, str2){
 
-function isAnagram(str1, str2) {
-
+  if(str1.length!=str2.length){
+    return false;
+  }
+  let arr1=str1.split("");
+  let arr2=str2.split("");
+  arr1.sort();
+  arr2.sort();
+  for(let i=0,j=0;i<str1.length;i++,j++){
+    if(arr1[i]!=arr2[j]){
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
